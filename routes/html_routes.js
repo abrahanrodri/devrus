cb5f6c8ee2fca25f3078227d92bb6ba37d3365c5
Abe <abrahan.rodri@gmail.com>
2019-08-11 14:42:45 -0400

    updating josh's routes

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -12,24 +12,37 @@ module.exports = function(app) {
 
   // Each of the below routes just handles the HTML page that the user gets sent to.
 
-  // index route loads view.html
+  // index route loads home.html
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
-  // cms route loads cms.html
-  app.get("/cms", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/cms.html"));
+  // employerview route loads employerview.html
+  app.get("/employerview", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/employerview.html"));
+  });
+   // employerPost route loads employerPost.html
+   app.get("/employerPost", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/employerPost.html"));
   });
 
-  // blog route loads blog.html
-  app.get("/blog", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/blog.html"));
+  // eventview route loads eventview.html
+  app.get("/eventview", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/eventview.html"));
   });
 
-  // authors route loads author-manager.html
-  app.get("/authors", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/author-manager.html"));
+    // event Post route loads eventPost.html
+    app.get("/eventPost", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/eventPost.html"));
+    });
+
+  // freelancer view route loads frelancerview.html
+  app.get("/freelancerview", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/freelancerview.html"));
+  });
+   // freelancer Post view route loads freePost.html
+   app.get("/freePost", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/freePost.html"));
   });
 
 };
